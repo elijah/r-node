29f079f1b9f79fb1bb1133deb9a998e51ed60154
Jamie Love <drjlove@gmail.com>
2010-04-04 09:26:32 +1200

    Plot now plots on bottom and left lines properly.

--- shared/js/R/graphs/r-plot.js
+++ shared/js/R/graphs/r-plot.js
@@ -108,7 +108,7 @@ rnode.graph.PlotDefault = Ext.extend (rnode.graph.Graph, {
         if (!config.small) {
             var yticks = yscale.ticks();
             if (yticks[0] != ymin)
-                yticks.unshift (ymin);
+                yticks.unshift (yscale.invert(0));
             vis.add (pv.Rule)
                 .data (yticks)
                 .left (-10)
@@ -119,7 +119,7 @@ rnode.graph.PlotDefault = Ext.extend (rnode.graph.Graph, {
                 .text (function (d) { return yscale.tickFormat(d) });
             var xticks = xscale.ticks();
             if (xticks[0] != xmin)
-                xticks.unshift (xmin);
+                xticks.unshift (xscale.invert(0));
             vis.add (pv.Rule)
                 .data (xticks)
                 .bottom (-10)
