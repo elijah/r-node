d73b61b4a8be910226aa74bdad860316738a534c
Jamie Love <drjlove@gmail.com>
2010-04-09 00:16:38 +1200

    Clarify and cleanup notes to run Rserve in a chroot jail.

--- rjail/Rserve.sh
+++ rjail/Rserve.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+#
+# For a Ubuntu setup. You may need to tweak this for other setups.
 export R_HOME=/usr/lib/R
+R_SHARE_DIR=/usr/share/R/share
+export R_SHARE_DIR
+R_INCLUDE_DIR=/usr/share/R/include
+export R_INCLUDE_DIR
+R_DOC_DIR=/usr/share/R/doc
+export R_DOC_DIR
+
 export TMPDIR=/tmp/R/
 export LD_LIBRARY_PATH=/usr/lib:/usr/lib/R/lib:/usr/lib/R
 exec /usr/lib/R/bin/Rserve
