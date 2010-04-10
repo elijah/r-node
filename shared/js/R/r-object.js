59ca030028cd7b13d80cb170b889e1528abc69bb
Jamie Love <drjlove@gmail.com>
2010-04-10 22:43:39 +1200

    Start using functional javascript by Oliver Steele http://osteele.com/sources/javascript/functional/

--- shared/js/R/r-object.js
+++ shared/js/R/r-object.js
@@ -25,23 +25,43 @@
   authors and should not be interpreted as representing official policies, either expressed
   or implied, of Jamie Love.
 */
+
+
 /**
- * An object representing a response from R. It provides access to all the internals,
+ * An object representing a response from R. It provides access to all the internals
+ * of the response, and provides some helper functions that help hide the exact return
+ * format.
+ *
+ * TODO: Define general valid formats of data that R will return, and what they mean
+ *       x.values vs. x.data vs. x.attributes
  *
  * It does not provide general display functionality, though it will convert the
  * data to a string (basically a JSON string) for debug purposes.
  */
-Ext.ns ('rnode.R');
 
+RNodeCore.ns ('rnode.R');
+
+/**
+ * Constructor.
+ *
+ * Parameters:
+ *      serverData    The data returned from the server, as an object (rather than
+ *                    in JSON format).
+ *
+ *      originalParsedCommand
+ *                    The original command that caused the response, or null if
+ *                    this is not relevant or available.
+ */
 rnode.R.RObject = function (serverData, originalParsedCommand) {
     if (!serverData) {
-        throw new Error ("rnode.R.RObject creating with null data.");
+        throw new Error ("rnode.R.RObject created with null server data.");
     }
     this.originalParsedCommand = originalParsedCommand;
     this.serverData = serverData;
 }
 
-rnode.R.RObject = Ext.extend (rnode.R.RObject, {
+rnode.R.RObject = RNodeCore.extend (rnode.R.RObject, {
+
     /**
      * Returns the class of the object, if it has one.
      */
@@ -50,21 +70,32 @@ rnode.R.RObject = Ext.extend (rnode.R.RObject, {
             return 'array';
         }
 
-        if (this.serverData && this.serverData.attributes && this.serverData.attributes.class) {
-            return this.serverData.attributes.class;
-        }
+        if (this.serverData) {
+            if (this.serverData.attributes && this.serverData.attributes.class) {
+                return this.serverData.attributes.class;
+            }
 
-        if (this.serverData.data) { // No class, but a data item == a list of data.
-            return 'list';
+            if (this.serverData.data) { // No class, but a data item == a list of data.
+                return 'list';
+            }
         }
 
         throw new Error ('Cannot figure out class for R object');
     },
 
+    /**
+     * Test if the R object is a plain array of data.
+     *
+     * Returns true when it is, false otherwise.
+     */
     isArray: function () {
-        return Ext.isArray(this.serverData);
+        return RNodeCore.isArray(this.serverData);
     },
 
+    /**
+     * Returns the R data as an array. It only does this if it is possible to
+     * present the R data as a single array of information.
+     */
     toArray: function () {
         if (!this.isArray())
             throw new Error ("rnode.R.RObject.toArray() called for non-array.");
@@ -73,30 +104,34 @@ rnode.R.RObject = Ext.extend (rnode.R.RObject, {
     },
 
     /**
-     * Finds data with the given name
+     * Attempts to find some data from the R response with the given name.
+     * It currently only looks at the returned R data.
+     *
+     * TODO: Expand on what can be searched.
+     *
+     * Parameters
+     *      n   The name of the data to return.
      */
     find: function (n) {
-        if (Ext.isArray(this.serverData)) {
-            return null;
-        }
-
-        if (!this.serverData.data) {
+        if (this.isArray() || !this.serverData || !this.serverData.data) {
             return null;
         }
 
         return this.serverData.data[n];
     },
 
+    /**
+     * Returns true if the server data includes named data 'n'.
+     *
+     * Parameters
+     *      n   The name of the data to return.
+     */
     has: function (n) {
         return this.find(n) != null;
     },
 
     data: function () {
-        if (Ext.isArray(this.serverData)) {
-            return null;
-        }
-
-        if (!this.serverData.data) {
+        if (!this.serverData || !this.serverData.data || RNodeCore.isArray(this.serverData)) {
             return null;
         }
 
---
+++ shared/js/r-node-core.js
@@ -0,0 +1,65 @@
+/*
+  Copyright 2010 Jamie Love. All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, are
+  permitted provided that the following conditions are met:
+
+     1. Redistributions of source code must retain the above copyright notice, this list of
+        conditions and the following disclaimer.
+
+     2. Redistributions in binary form must reproduce the above copyright notice, this list
+        of conditions and the following disclaimer in the documentation and/or other materials
+        provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY JAMIE LOVE ``AS IS'' AND ANY EXPRESS OR IMPLIED
+  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL JAMIE LOVE OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+  The views and conclusions contained in the software and documentation are those of the
+  authors and should not be interpreted as representing official policies, either expressed
+  or implied, of Jamie Love.
+*/
+
+
+
+/**
+ * Provides core functions for javascript code, to ensure that all R-Node shared code does
+ * not rely on a single underlying library.
+ */
+
+RNodeCore = function () {
+    return {
+
+        /**
+         * Creates a namespace, if one doesn't already exist, at the top level. Format is
+         * dot notation, just like ExtJS supports.
+         */
+        ns: function (s) {
+            return Ext.ns(s);
+        },
+
+        /**
+         * Extends an object with the properties and functions of one or more additional
+         * objects. This extends the object prototype, having the same semantics as
+         * Ext.extend from ExtJS.
+         */
+        extend: function () {
+            var args = Array.prototype.slice.call(arguments);
+            return Ext.extend.apply (this, args);
+        },
+
+        /**
+         * Tests whether the given object is a real javascript array or not, or at least
+         * fakes one well enough to pass as an array.
+         */
+        isArray: function (a) {
+            return Ext.isArray (a);
+        }
+    };
+}();
